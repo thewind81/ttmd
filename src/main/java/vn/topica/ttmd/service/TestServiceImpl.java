aa3ebf481bf7ce408abf0d60350b53e0d8c24849
TrongHH <tronghh@topica.edu.vn>
2019-03-26 15:37:31 +0700

    TrongHH

--- src/main/java/vn/topica/ttmd/service/TestServiceImpl.java
+++ src/main/java/vn/topica/ttmd/service/TestServiceImpl.java
@@ -1,12 +1,14 @@
 package vn.topica.ttmd.service;
 
-import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import vn.topica.ttmd.bo.Test;
 
+import javax.transaction.Transactional;
+
 @Service
+@Transactional
 public class TestServiceImpl implements TestService {
     @Autowired
     private SessionFactory sessionFactory;
@@ -15,8 +17,6 @@ public class TestServiceImpl implements TestService {
     public Test doInsert(Test test) {
         try {
             sessionFactory.getCurrentSession().save(test);
-        } catch (HibernateException e) {
-
         } finally {
             return test;
         }
